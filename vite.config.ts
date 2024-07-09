8fe803dc5683db1856d44fe28e1d415f96eeb02d
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2024-07-09 16:02:01 -0300

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,4 +12,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
