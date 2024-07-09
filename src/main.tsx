cfc87c1468a391c063f272ff728fe6cdfcaf63a2
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2024-07-09 20:59:58 -0300

    feat: add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 
-import { App } from '@/App'
+import { App } from '@/app'
 import { GlobalStyles, theme } from '@/styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
