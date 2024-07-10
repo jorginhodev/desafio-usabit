58525cadaa836b51bb1f6a0ed71bd5c439f729d6
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2024-07-10 14:42:20 -0300

    feat: update default layout with header component

--- src/pages/_layouts/default/index.tsx
+++ src/pages/_layouts/default/index.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components'
+
+import { Container } from './styles'
+
 export function DefaultLayout() {
   return (
-    <div>
-      <header>
-        <h1>Cabeçalho</h1>
-      </header>
+    <Container>
+      <Header />
 
       <main>
         <Outlet />
       </main>
-    </div>
+    </Container>
   )
 }
