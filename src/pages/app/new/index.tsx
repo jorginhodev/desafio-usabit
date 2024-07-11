b57d8e7864d31e7772a0916de952ee078b378bf1
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2024-07-10 23:55:14 -0300

    feat: add form validation and submission logic to create new customer

--- src/pages/app/new/index.tsx
+++ src/pages/app/new/index.tsx
@@ -1,14 +1,84 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { ArrowLeft } from 'lucide-react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { z } from 'zod'
+
+import { createCustomer } from '@/api/create-customer'
+import { Button, Input } from '@/components'
 
 import * as S from './styles'
 
+const schema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  city: z.string().min(3),
+  state: z.string().min(2),
+})
+
+type FormData = z.infer<typeof schema>
+
 export function New() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isValid },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  })
+
+  const navigate = useNavigate()
+
+  const { mutateAsync: createCustomerFn } = useMutation({
+    mutationFn: createCustomer,
+  })
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      await createCustomerFn({
+        name: data.name,
+        email: data.email,
+        city: data.city,
+        state: data.state,
+      })
+
+      toast.success('Cliente cadastrado com sucesso!')
+      navigate('/')
+    } catch (error) {
+      toast.error('Erro ao cadastrar cliente.')
+    }
+  }
+
   return (
     <S.Container>
-      <S.Header to="/">
+      <S.BackButton to="/">
         <ArrowLeft />
         <span>Voltar</span>
-      </S.Header>
+      </S.BackButton>
+
+      <S.Title>Novo Cliente</S.Title>
+
+      <S.Form onSubmit={handleSubmit(onSubmit)}>
+        <Input placeholder="Nome" required {...register('name')} />
+        <Input
+          placeholder="Email"
+          required
+          type="email"
+          {...register('email')}
+        />
+        <Input placeholder="Cidade" required {...register('city')} />
+        <Input placeholder="Estado" required {...register('state')} />
+
+        <Button
+          type="submit"
+          variant="primary"
+          disabled={!isValid || isSubmitting}
+        >
+          Cadastrar
+        </Button>
+      </S.Form>
     </S.Container>
   )
 }
--- src/pages/app/new/styles.ts
+++ src/pages/app/new/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   margin-top: 32px;
 `
 
-export const Header = styled(Link)`
+export const BackButton = styled(Link)`
   display: flex;
   align-items: center;
   gap: 8px;
@@ -20,4 +20,28 @@ export const Header = styled(Link)`
   &:hover {
     opacity: 0.8;
   }
+`
+
+export const Title = styled.h1`
+  margin-top: 8px;
+  font-size: 24px;
+  font-weight: bold;
+`
+
+export const Form = styled.form`
+  margin-top: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  & > button {
+    width: 100%;
+    height: 52px;
+  }
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
 `
