063e250aa54d6a18fd268a25727e241a4391abeb
Jorge Bierhals Ramos <jorginhodeveloper@gmail.com>
2024-07-10 23:54:01 -0300

    feat: Add React Toastify and ToastContainer components for displaying notifications

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,8 @@
+import 'react-toastify/dist/ReactToastify.css'
+
 import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import { queryClient } from '@/lib/react-query'
 
@@ -9,6 +12,8 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+
+      <ToastContainer />
     </QueryClientProvider>
   )
 }
